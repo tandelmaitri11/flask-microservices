cecd3a4cdc5e6a34bfd936423816bd5b95c2874a
tandelmaitri11 <tandelmaitri11@gmail.com>
2025-09-13 15:47:19 +0530

    Add new root

--- web-service/app.py
+++ web-service/app.py
@@ -13,6 +13,10 @@ def home():
 @app.route('/about')
 def about():
 	return "Web Service in Microservice Demo"
+@app.route('/newRoute')
+def newRoute():
+	return "This is a new route"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=5000)
+
